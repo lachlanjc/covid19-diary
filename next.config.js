c074fffdbc150dc33251124d790ce47f5226f281
Glitch (hackclub-theme-starter) <none>
2020-05-18 07:27:28 +0000

    🌮👱 Checkpoint ./lib/theme.js:1241665/241 ./pages/index.mdx:1241665/316 ./next.config.js:1241665/69

--- next.config.js
+++ next.config.js
@@ -1,2 +1,5 @@
 const withMDX = require('@next/mdx')({ extension: /\.mdx?$/ })
-module.exports = withMDX({ pageExtensions: ['js', 'jsx', 'mdx'] })
+module.exports = withMDX({
+  pageExtensions: ['js', 'jsx', 'mdx'],
+  devIndicators: { autoPrerender: false }
+})
